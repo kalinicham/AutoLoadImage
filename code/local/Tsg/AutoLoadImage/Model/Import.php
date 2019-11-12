c56a32611c0904b67c8cafff6be4bfe15ad978cf
kalinicham <kalinicham@ukr.net>
2019-11-12 23:24:05 +0200

    Import

--- code/local/Tsg/AutoLoadImage/Model/Import.php
+++ code/local/Tsg/AutoLoadImage/Model/Import.php
@@ -9,12 +9,16 @@ class Tsg_AutoLoadImage_Model_Import extends Mage_Core_Model_Abstract
         $this->_init('tsg_autoloadimage/import');
     }
 
-
-
     public function importSource()
     {
-        /** @var Tsg_AutoLoadImage_Helper_Check $helperCheck */
+        /**
+         * @var Tsg_AutoLoadImage_Helper_Check $helperCheck
+         * @var Tsg_AutoLoadImage_Helper_Data $helperData
+         * @var Mage_Catalog_Model_Product $product
+         */
+
         $helperCheck = Mage::helper('tsg_autoloadimage/check');
+        $helperData = Mage::helper('tsg_autoloadimage/data');
 
         $uploadedFile = $_FILES['filename']['tmp_name'];
         $handle = fopen($uploadedFile, "r");
@@ -26,10 +30,7 @@ class Tsg_AutoLoadImage_Model_Import extends Mage_Core_Model_Abstract
             return;
         }
 
-        /** @var array|false|null $headFile */
         $headFile = fgetcsv($handle, 1000, ",");
-
-        /** @var string $nonExist */
         $nonExist = $helperCheck->checkField($headFile);
 
         if ($nonExist !== '')
@@ -41,9 +42,10 @@ class Tsg_AutoLoadImage_Model_Import extends Mage_Core_Model_Abstract
             return;
         }
 
-        /** @var Mage_Catalog_Model_Product $product */
         $product = Mage::getModel('catalog/product');
+        $numberLine = 0;
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $numberLine++;
             /** @var Tsg_AutoLoadImage_Model_Import $importData */
             $importData = Mage::getModel('tsg_autoloadimage/import');
             $item = 0;
@@ -51,17 +53,30 @@ class Tsg_AutoLoadImage_Model_Import extends Mage_Core_Model_Abstract
                 $importData->setData($value,$data[$item]);
                 $item++;
             }
-            $id = $product->getIdBySku($importData->getSku());
+            $sku = $importData->getSku();
+            $id = $product->getIdBySku($sku);
             if ($id) {
-               $checkLink = $helperCheck->checkLink($importData->getLink());
+                $checkLink = $helperCheck->checkLink($importData->getLink());
                 if ($checkLink) {
                     $importData->setData('id_product',$id);
                     $importData->setData('start_datetime',Mage::getModel('core/date')->gmtDate('Y-m-d H:i:s'));
                     $importData->save();
+                }else{
+                    Mage::getSingleton('adminhtml/session')->addError(
+                        $helperData->__("Error line: $numberLine - The link in the is not an image")
+                    );
+//                Mage::log("Error line: $numberLine - The link in the is not an image",null,'tsg_import.log');
                 }
+            }else{
+
+                Mage::getSingleton('adminhtml/session')->addError(
+                    $helperData->__("Error line: $numberLine - Unknown value $sku")
+                );
+//                Mage::log("Error line: $numberLine - Unknown value $sku",null,'tsg_import.log');
             }
         }
         fclose($handle);
+        Mage::getSingleton('adminhtml/session')->addSuccess('The file imported.');
     }
 
 }
